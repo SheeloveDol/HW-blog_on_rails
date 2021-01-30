3cb6382f300b37bf352357d5bea7320a5020844c
Sheelove Dol <siesel26@gmail.com>
2021-01-30 13:23:07 -0800

    Add Post routes, model, controller, and seed files with faker gem

--- Gemfile
+++ Gemfile
@@ -42,6 +42,9 @@ group :development do
   gem 'listen', '~> 3.3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+  gem 'cowsay', '~> 0.3'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all()
+
+50.times do
+  created_at = Faker::Date.backward(365)
+  Post.create({
+    title: Faker::Games::Witcher.witcher,
+    body: Faker::Games::Witcher.quote,
+    created_at: created_at,
+    updated_at: created_at
+  })
+end
+
+puts Cowsay.say("Created 50 posts!", :frogs)
