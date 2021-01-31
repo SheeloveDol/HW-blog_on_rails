0d1c5c37e60b3098d3789dfb17b26ef10174aa60
Sheelove Dol <siesel26@gmail.com>
2021-01-31 01:14:24 -0800

    Add users controller and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+
   root 'posts#index'
   resources :posts do 
     resources :comments, only: [:create, :destroy]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:new, :create]
+  
 end
