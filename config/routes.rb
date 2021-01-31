dea42a0b8bfe0156da0bf75cc286db4645626cc0
Sheelove Dol <siesel26@gmail.com>
2021-01-31 01:47:18 -0800

    Add session controller and implement create and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
+  resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create] do 
+    delete :destroy, on: :collection
+  end
+  
   root 'posts#index'
   resources :posts do 
     resources :comments, only: [:create, :destroy]
   end
 
-  resources :users, only: [:new, :create]
   
 end
